a88861b1ba1e58b14a5117d98dbca2dfa7407f7d
Andre-Favero <andrefaverofacul@gmail.com>
2023-03-29 17:18:41 -0300

    testando push do terminal, pois estava com problema, obrigado luigidt por tentar me impedir, sem mãe

--- src/controllers/PageController.ts
+++ src/controllers/PageController.ts
@@ -4,8 +4,14 @@ export const home = (req: Request, res: Response) => {
   res.send("home do controller");
 };
 
-export const dogs = (req: Request, res: Response) => {};
+export const dogs = (req: Request, res: Response) => {
+  res.send("catioro");
+};
 
-export const cats = (req: Request, res: Response) => {};
+export const cats = (req: Request, res: Response) => {
+  res.send("gatito");
+};
 
-export const fishes = (req: Request, res: Response) => {};
+export const fishes = (req: Request, res: Response) => {
+  res.send("pexe");
+};
